a688b99aa1b9f199d368a762543c275aa689c333
CrystalWolf5 <241708711+CrystalWolf5@users.noreply.github.com>
2026-05-12 16:51:59 +0200

    Agrego el botón de añadir nuevos Estudios

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -25,3 +25,43 @@ botonTema.addEventListener("click", () => {
     }
 
 });
+
+// Boton para mostrar el formulario de añadir estudios y para agregar un nuevo estudio a la lista
+
+const botonMostrar = document.getElementById("mostrarFormulario");
+const formulario = document.getElementById("formularioEstudio");
+const botonAgregar = document.getElementById("agregarEstudio");
+const lista = document.getElementById("listaEstudios");
+const input = document.getElementById("nuevoEstudio");
+
+botonMostrar.addEventListener("click", () => {
+    if (formulario.style.display === "none") {
+        formulario.style.display = "block";
+    } else {
+        formulario.style.display = "none";
+    }
+});
+
+botonAgregar.addEventListener("click", () => {
+    const texto = input.value;
+
+    if (texto !== "") {
+        const nuevoLi = document.createElement("li");
+        nuevoLi.textContent = texto + " ";
+
+        const botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+
+        botonEliminar.addEventListener("click", () => {
+            nuevoLi.remove();
+        });
+
+        nuevoLi.appendChild(botonEliminar);
+
+        lista.appendChild(nuevoLi);
+
+        input.value = "";
+
+        formulario.style.display = "none";
+    }
+});
