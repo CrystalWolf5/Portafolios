51fbcd9abf745250efee364d74d03ed3fd77bc79
CrystalWolf5 <241708711+CrystalWolf5@users.noreply.github.com>
2026-05-14 12:57:55 +0200

    Mejora seccion GitHub

--- scripts/api.js
+++ scripts/api.js
@@ -8,7 +8,6 @@ async function buscarUsuario() {
 
     document.getElementById("avatar").src = data.avatar_url;
     document.getElementById("name").textContent = data.name || data.login;
-    document.getElementById("bio").textContent = data.bio || "Sin bio disponible";
     document.getElementById("repos").textContent = data.public_repos;
 
     // Obtener los repositorios del usuario
@@ -19,15 +18,50 @@ async function buscarUsuario() {
 
     const repoList = document.getElementById("repo-list");
 
-    reposData.forEach(repo => {
+    const languageTotals = {};
+
+    for (const repo of reposData) {
 
         const li = document.createElement("li");
 
-        li.innerHTML = `${repo.name} <button onclick="window.open('${repo.html_url}', '_blank')"> Ver repositorio </button>`;
+        li.innerHTML = `
+        <span class="repo-name">${repo.name}</span> 
+        <button onclick="window.open('${repo.html_url}', '_blank')"> Ver repositorio </button>`;
 
         repoList.appendChild(li);
 
-    });
+        const languagesDiv = document.getElementById("languages");
+
+        const languageTotals = {};
+
+        const colors = {JavaScript: "#f7df1e", HTML: "#e34c26", CSS: "#6c00be"};
+
+        let totalBytes = 0;
+
+        for (const repo of reposData) {
+            const langResponse = await fetch(repo.languages_url);
+            const langData = await langResponse.json();
+
+            for (const lang in langData) {
+                const value = langData[lang];
+                languageTotals[lang] = (languageTotals[lang] || 0) + value;
+                totalBytes += value;
+            }
+        }
+
+        for (const lang in languageTotals) {
+
+            const percent = (languageTotals[lang] / totalBytes) * 100;
+            const langItem = document.createElement("div");
+            langItem.classList.add("language-item");
+            langItem.innerHTML = `<p class="lang-text ${lang}">${lang} - ${percent.toFixed(1)}% </p>
+            <div class="bar">
+            <div class="fill ${lang}" style="width:${percent}%; background:${colors[lang]}"></div>
+            </div>`;
+
+            languagesDiv.appendChild(langItem);
+        }
+    }
 
 }
 
